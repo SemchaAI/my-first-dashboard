6a34d80fa0266e2cb9f709837e6d937e75194221
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-04-18 16:33:58 +0300

    feact: sign-in functionality on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { SignInForm } from "@/components/entities";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <section className="flex h-dvh w-full flex-col">
+      <h1>Home</h1>
+      <SignInForm />
+    </section>
+  );
 }
