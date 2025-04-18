299aa9157551785c08ea900bd4e6659987f399fa
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-04-18 18:39:10 +0300

    refactor: updated config file
    -added centralized role data to columns array
    -fixed representation of role based column in this table row

--- src/app/(dashboard)/list/announcements/tableConfig.tsx
+++ src/app/(dashboard)/list/announcements/tableConfig.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import { Eye, Trash } from "lucide-react";
-import type { Role } from "@prisma/client";
-import type { AnnouncementList } from "@/utils/models/tables";
+import { Role } from "@prisma/client";
+import type { AnnouncementList, IColumn } from "@/utils/models/tables";
 
-const columns = [
+const columns: IColumn[] = [
   {
     header: "Title",
     accessor: "title",
@@ -20,6 +20,7 @@ const columns = [
   {
     header: "Actions",
     accessor: "action",
+    role: Role.ADMIN,
   },
 ];
 
@@ -33,21 +34,22 @@ const renderRow = (item: AnnouncementList, role: Role) => (
     <td className="hidden px-2 py-0.5 md:table-cell">
       {item.date.toLocaleDateString()}
     </td>
-    <td>
-      <div className="flex items-center gap-2">
-        <Link
-          href={`/list/teachers/${item.id}`}
-          className="flex h-7 w-7 cursor-pointer items-center justify-center rounded-full bg-primary"
-        >
-          <Eye size={16} className="stroke-background" />
-        </Link>
-        {role === "ADMIN" && (
+    {role === Role.ADMIN && (
+      <td>
+        <div className="flex items-center gap-2">
+          <Link
+            href={`/list/teachers/${item.id}`}
+            className="flex h-7 w-7 cursor-pointer items-center justify-center rounded-full bg-primary"
+          >
+            <Eye size={16} className="stroke-background" />
+          </Link>
+
           <button className="flex h-7 w-7 cursor-pointer items-center justify-center rounded-full bg-secondary">
             <Trash size={16} className="stroke-background" />
           </button>
-        )}
-      </div>
-    </td>
+        </div>
+      </td>
+    )}
   </tr>
 );
 
