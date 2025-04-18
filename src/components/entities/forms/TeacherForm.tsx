d03425243f28ef3a12b276f31f0c7e4a41d305c6
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-04-18 18:22:31 +0300

    chore: temporally removed teacherForm

--- src/components/entities/forms/TeacherForm.tsx
+++ src/components/entities/forms/TeacherForm.tsx
@@ -1,43 +1,43 @@
-"use client";
-import { useForm } from "react-hook-form";
-import { Form } from "./Form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { type TeacherSchema, teacherSchema } from "@/utils/config";
-import { InputField } from "@/components/features";
-import { User } from "lucide-react";
+// "use client";
+// import { useForm } from "react-hook-form";
+// import { Form } from "./Form";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { type TeacherSchema, teacherSchema } from "@/utils/config";
+// import { InputField } from "@/components/features";
+// import { User } from "lucide-react";
 
-interface IProps {
-  type: "create" | "update";
-  data?: any;
-}
+// interface IProps {
+//   type: "create" | "update";
+//   data?: any;
+// }
 
-export const TeacherForm = ({ type, data }: IProps) => {
-  const form = useForm({
-    resolver: zodResolver(teacherSchema),
-    defaultValues: data,
-  });
-  const submitHandler = async (data: TeacherSchema) => {
-    //const { email, name, password } = data;
-    console.log("data", data);
-    try {
-      console.log("TeacherForm", data);
-    } catch (error) {
-      console.log("[TeacherForm]", error);
-    }
-  };
+// export const TeacherForm = ({ type, data }: IProps) => {
+//   const form = useForm({
+//     resolver: zodResolver(teacherSchema),
+//     defaultValues: data,
+//   });
+//   const submitHandler = async (data: TeacherSchema) => {
+//     //const { email, name, password } = data;
+//     console.log("data", data);
+//     try {
+//       console.log("TeacherForm", data);
+//     } catch (error) {
+//       console.log("[TeacherForm]", error);
+//     }
+//   };
 
-  return (
-    <Form form={form} onSubmit={submitHandler}>
-      {
-        // inputs
-        <InputField id="username" label="Username" type="text" Icon={User} />
-      }
-      {
-        // formControls
-        <div>
-          <button type="submit">Click</button>
-        </div>
-      }
-    </Form>
-  );
-};
+//   return (
+//     <Form form={form} onSubmit={submitHandler}>
+//       {
+//         // inputs
+//         <InputField id="username" label="Username" type="text" Icon={User} />
+//       }
+//       {
+//         // formControls
+//         <div>
+//           <button type="submit">Click</button>
+//         </div>
+//       }
+//     </Form>
+//   );
+// };
