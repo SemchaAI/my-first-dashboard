9f850919c46721acf375e82186a348ed9961874f
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-04-23 13:08:22 +0300

    chore: in future i will try to systemize basic popular errors

---
+++ src/utils/config/errors.ts
@@ -0,0 +1,13 @@
+export class ApiError extends Error {
+  status: number;
+  constructor(message: string, status = 500) {
+    super(message);
+    this.status = status;
+  }
+}
+
+export class UnauthorizedError extends ApiError {
+  constructor(message = "Unauthorized") {
+    super(message, 401);
+  }
+}
