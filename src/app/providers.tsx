90136b4f7ed780727e29124fe609ab1886725af8
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-04-23 13:02:47 +0300

    feat: added toast in providers with config

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,14 @@
 "use client";
-// import { toastOptions } from '@/utils/consts/ToastConfig';
 import { SessionProvider } from "next-auth/react";
-// import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
+
+import { toastOptions } from "@/utils/config";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       {children}
-      {/* <Toaster position="top-left" toastOptions={toastOptions} /> */}
+      <Toaster position="top-right" toastOptions={toastOptions} />
     </SessionProvider>
   );
 }
---
+++ src/utils/config/toastConfig.ts
@@ -0,0 +1,3 @@
+export const toastOptions = {
+  className: "border border-border p-2 text-text-primary bg-background",
+};
