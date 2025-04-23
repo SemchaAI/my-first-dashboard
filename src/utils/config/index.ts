d42e644c2770153dc52d9b45dcf966d5773c3976
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-04-23 13:04:17 +0300

    chore: reexport

--- src/utils/config/index.ts
+++ src/utils/config/index.ts
@@ -4,5 +4,13 @@ export { menu } from "./menu";
 export { signInSchema } from "./schemas";
 export type { SignInSchema } from "./schemas";
 
-//auth
+//routes
 export { routeAccessMap } from "./routes";
+export { API_ROUTES } from "./routes";
+
+//errors
+export { UnauthorizedError } from "./errors";
+export { ApiError } from "./errors";
+
+//toast
+export { toastOptions } from "./toastConfig";
