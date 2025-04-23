884f432fb1a5e451a71f110b64d2f3f79ed13eed
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-04-23 13:06:16 +0300

    feat: strict server side component with role based announcements

--- src/components/entities/calendars/Announcements.tsx
+++ src/components/entities/calendars/Announcements.tsx
@@ -1,32 +1,29 @@
+"use server";
 import { Header } from "@/components/features";
+import { prisma } from "@/prisma/prismaClient";
+import { getUserSession } from "@/utils/helpers";
+import { Role } from "@prisma/client";
 
-// interface IProps {}
+export const Announcements = async () => {
+  const user = await getUserSession();
+  if (!user) return null;
 
-const data = [
-  {
-    id: 1,
-    title: "Announcement 1",
-    date: "2025-01-01",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, quae.",
-  },
-  {
-    id: 2,
-    title: "Announcement 2",
-    date: "2025-01-01",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, quae.",
-  },
-  {
-    id: 3,
-    title: "Announcement 3",
-    date: "2025-01-01",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, quae.",
-  },
-];
+  const roleConditions = {
+    TEACHER: { lessons: { some: { teacherId: user.id } } },
+    STUDENT: { students: { some: { id: user.id } } },
+    PARENT: { students: { some: { parentId: user.id } } },
+  };
 
-export const Announcements = () => {
+  const data = await prisma.announcement.findMany({
+    take: 3,
+    orderBy: { date: "desc" },
+    where: {
+      ...(user.role !== Role.ADMIN && {
+        OR: [{ classId: null }, { class: roleConditions[user.role] || {} }],
+      }),
+    },
+  });
+
   return (
     <div className="flex w-full flex-col gap-1 rounded-2xl bg-background p-4">
       <Header title="Announcements" linkText="View all" link="#" />
@@ -34,14 +31,14 @@ export const Announcements = () => {
         {data.map((item) => (
           <div
             key={item.id}
-            className="relative flex flex-col overflow-hidden rounded-xl p-4 odd:bg-secondary even:bg-primary"
+            className="relative flex flex-col overflow-hidden rounded-xl p-4 odd:bg-secondary even:bg-primary nth-[3n]:bg-pink-200"
           >
             <div className="flex flex-wrap items-center justify-between gap-1 text-nowrap">
               <h1 className="text-lg font-semibold text-text-highlight">
                 {item.title}
               </h1>
               <span className="rounded-2xl bg-background px-2 py-1 text-sm leading-3.5">
-                {item.date}
+                {item.date.toLocaleDateString()}
               </span>
             </div>
             <p>{item.description}</p>
