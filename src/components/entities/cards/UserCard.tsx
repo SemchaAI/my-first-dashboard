0e85700dd3afcdf1b9c8562d6f21094fefcaf9b7
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-04-23 13:05:35 +0300

    feat: now userCard get all data from db

--- src/components/entities/cards/UserCard.tsx
+++ src/components/entities/cards/UserCard.tsx
@@ -2,20 +2,23 @@ import { Ellipsis } from "lucide-react";
 
 interface IProps {
   type: string;
+  date: string;
+  total: number;
 }
 
-export const UserCard = ({ type }: IProps) => {
+export const UserCard = ({ type, date, total }: IProps) => {
   return (
     <li className="min-w-32 flex-1 rounded-2xl p-4 odd:bg-secondary even:bg-tertiary">
       <div className="flex grow items-center justify-between">
         <span className="rounded-full bg-white px-2 py-1 text-xs text-success">
-          2025/25
+          {date}
         </span>
         <Ellipsis size={24} className="cursor-pointer stroke-white" />
       </div>
-      <h1 className="my-4 text-2xl font-bold text-text-highlight">1,234</h1>
+      <h1 className="my-4 text-2xl font-bold text-text-highlight">{total}</h1>
       <p className="text-sm font-medium text-text-primary capitalize">
-        {type + "s"}
+        {type}
+        {total > 1 && "s"}
       </p>
     </li>
   );
