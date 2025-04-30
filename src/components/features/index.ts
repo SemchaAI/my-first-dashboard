5e69c56a57fd2a815021bb5d1edb0e1b4bb22968
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-04-30 16:22:22 +0300

    chore: reexport

--- src/components/features/index.ts
+++ src/components/features/index.ts
@@ -2,8 +2,10 @@ export { Badge } from "./badge/Badge";
 export { Header } from "./card/Header";
 export { Search } from "./search/Search";
 export { Pagination } from "./pagination/Pagination";
-export { FormModal } from "./modal/form/FormModal";
+export { ModalWithTrigger } from "./modal/ModalWithTrigger";
 
 export { InputField } from "./fields/InputField";
+export { MultiSelect } from "./selects/MultiSelect";
+export { MultiSelectField } from "./selects/MultiSelectField";
 
 export { UserButton } from "./user/UserButton";
