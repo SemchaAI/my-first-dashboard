4b4d8b3abd6d152af824e2f2d190db4b5f69c875
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-04-30 16:05:35 +0300

    feat: types for other fields in forms (checkbox, multiselect)

--- src/utils/models/inputs.ts
+++ src/utils/models/inputs.ts
@@ -1,3 +1,5 @@
+import type { ReactNode } from "react";
+
 export interface IInput extends React.InputHTMLAttributes<HTMLInputElement> {
   rounded?: boolean;
 }
@@ -6,7 +8,35 @@ export interface IField extends IInput {
   EyeIcon?: boolean;
   Icon: React.FC<React.SVGProps<SVGSVGElement>>;
 
+  hidden?: boolean;
   label: string;
   type: string;
   placeholder?: string;
+}
+
+export interface ICheckbox
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange"> {
+  name: string;
+  endAdornment?: ReactNode;
+  onChange?: (
+    value: string | number | readonly string[] | undefined | boolean | null,
+  ) => void;
+  checked?: boolean;
+  label: string;
+  error?: string;
+}
+
+export interface IMultiSelectOption {
+  label: string;
+  value: string;
+  disabled?: boolean;
+}
+export interface IMultiSelect {
+  options: IMultiSelectOption[];
+  value: string[];
+  onChange: (value: string) => void;
+  onClear: () => void;
+  placeholder?: string;
+  loading?: boolean;
+  single?: boolean;
 }
