c0b1c08c475422fb6533fef29be04d930cbc0e92
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-04-30 16:09:08 +0300

    feat: subjectSchema + types , removed from comms TeacherSchema

--- src/utils/config/schemas.ts
+++ src/utils/config/schemas.ts
@@ -1,43 +1,48 @@
 import { z } from "zod";
 
-// const teacherSchema = z.object({
-//   username: z
-//     .string()
-//     .min(5, { message: "Username must be at least 5 characters long" })
-//     .max(15, { message: "Username must be at most 15 characters long" }),
-//   email: z.string().email({ message: "Invalid email address" }),
-//   password: z
-//     .string()
-//     .min(5, { message: "Password must be at least 5 characters long" })
-//     .max(255, { message: "Password must be at most 255 characters long" }),
-//   // .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" }),
-//   firstName: z
-//     .string()
-//     .min(2, { message: "First name must be at least 2 characters long" })
-//     .max(20, { message: "First name must be at most 20 characters long" }),
-//   lastName: z
-//     .string()
-//     .min(2, { message: "Last name must be at least 2 characters long" })
-//     .max(20, { message: "Last name must be at most 20 characters long" }),
-//   phone: z
-//     .string()
-//     .min(10, { message: "Phone number must be at least 10 digits long" }),
-//   address: z.string().min(1, { message: "Address is required" }),
-//   birthday: z.coerce.date({ message: "Birthday is required" }),
-//   sex: z.enum(["male", "female"], { message: "Sex is required" }),
-//   // img: z.instanceof(File, { message: "Image is required" }),
-//   img: z.string().optional(),
-// });
-// type TeacherSchema = z.infer<typeof teacherSchema>;
+export const teacherSchema = z.object({
+  username: z
+    .string()
+    .min(5, { message: "Username must be at least 5 characters long" })
+    .max(15, { message: "Username must be at most 15 characters long" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(5, { message: "Password must be at least 5 characters long" })
+    .max(255, { message: "Password must be at most 255 characters long" }),
+  // .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" }),
+  firstName: z
+    .string()
+    .min(2, { message: "First name must be at least 2 characters long" })
+    .max(20, { message: "First name must be at most 20 characters long" }),
+  lastName: z
+    .string()
+    .min(2, { message: "Last name must be at least 2 characters long" })
+    .max(20, { message: "Last name must be at most 20 characters long" }),
+  phone: z
+    .string()
+    .min(10, { message: "Phone number must be at least 10 digits long" }),
+  address: z.string().min(1, { message: "Address is required" }),
+  birthday: z.coerce.date({ message: "Birthday is required" }),
+  sex: z.enum(["male", "female"], { message: "Sex is required" }),
+  // img: z.instanceof(File, { message: "Image is required" }),
+  img: z.string().optional(),
+});
+export type TeacherSchema = z.infer<typeof teacherSchema>;
 
-const signInSchema = z.object({
+export const signInSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
   password: z
     .string()
     .min(5, { message: "Password must be at least 5 characters long" })
     .max(20, { message: "Password must be at most 255 characters long" }),
 });
-type SignInSchema = z.infer<typeof signInSchema>;
+export type SignInSchema = z.infer<typeof signInSchema>;
 
-export { signInSchema };
-export type { SignInSchema };
+export const subjectSchema = z.object({
+  id: z.coerce.number().optional(),
+  name: z.string().min(1, { message: "Subject name is required!" }),
+  teachers: z.array(z.string()), //teacher ids
+});
+
+export type SubjectSchema = z.infer<typeof subjectSchema>;
