2d48fbb8f28e45d04a725ca4297edddb9393b6ca
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-04-30 16:26:14 +0300

    chore: removed mock data from component( now use db data)

--- src/components/entities/calendars/BigCalendar.tsx
+++ src/components/entities/calendars/BigCalendar.tsx
@@ -3,157 +3,12 @@ import { useState } from "react";
 import { Calendar, momentLocalizer, type View } from "react-big-calendar";
 import moment from "moment";
 
+import type { IEvent } from "@/utils/models/calendar";
+
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
-// import "react-big-calendar/lib/addons/dragAndDrop/styles";
-
-export const calendarEvents = [
-  {
-    title: "Math",
-    allDay: false,
-    start: new Date(2025, 2, 31, 8, 0),
-    end: new Date(2025, 2, 31, 8, 45),
-  },
-  {
-    title: "English",
-    allDay: false,
-    start: new Date(2025, 2, 31, 9, 0),
-    end: new Date(2025, 2, 31, 9, 45),
-  },
-  {
-    title: "Biology",
-    allDay: false,
-    start: new Date(2025, 2, 31, 10, 0),
-    end: new Date(2025, 2, 31, 10, 45),
-  },
-  {
-    title: "Physics",
-    allDay: false,
-    start: new Date(2025, 2, 31, 11, 0),
-    end: new Date(2025, 2, 31, 11, 45),
-  },
-  {
-    title: "Chemistry",
-    allDay: false,
-    start: new Date(2025, 2, 31, 13, 0),
-    end: new Date(2025, 2, 31, 13, 45),
-  },
-  {
-    title: "History",
-    allDay: false,
-    start: new Date(2025, 2, 31, 14, 0),
-    end: new Date(2025, 2, 31, 14, 45),
-  },
-  {
-    title: "English",
-    allDay: false,
-    start: new Date(2025, 3, 1, 9, 0),
-    end: new Date(2025, 3, 1, 9, 45),
-  },
-  {
-    title: "Biology",
-    allDay: false,
-    start: new Date(2025, 3, 1, 10, 0),
-    end: new Date(2025, 3, 1, 10, 45),
-  },
-  {
-    title: "Physics",
-    allDay: false,
-    start: new Date(2025, 3, 1, 11, 0),
-    end: new Date(2025, 3, 1, 11, 45),
-  },
-
-  {
-    title: "History",
-    allDay: false,
-    start: new Date(2025, 3, 1, 14, 0),
-    end: new Date(2025, 3, 1, 14, 45),
-  },
-  {
-    title: "Math",
-    allDay: false,
-    start: new Date(2025, 3, 2, 8, 0),
-    end: new Date(2025, 3, 2, 8, 45),
-  },
-  {
-    title: "Biology",
-    allDay: false,
-    start: new Date(2025, 3, 2, 10, 0),
-    end: new Date(2025, 3, 2, 10, 45),
-  },
-
-  {
-    title: "Chemistry",
-    allDay: false,
-    start: new Date(2025, 3, 2, 13, 0),
-    end: new Date(2025, 3, 2, 13, 45),
-  },
-  {
-    title: "History",
-    allDay: false,
-    start: new Date(2025, 3, 2, 14, 0),
-    end: new Date(2025, 3, 1, 14, 45),
-  },
-  {
-    title: "English",
-    allDay: false,
-    start: new Date(2025, 3, 3, 9, 0),
-    end: new Date(2025, 3, 3, 9, 45),
-  },
-  {
-    title: "Biology",
-    allDay: false,
-    start: new Date(2025, 3, 3, 10, 0),
-    end: new Date(2025, 3, 3, 10, 45),
-  },
-  {
-    title: "Physics",
-    allDay: false,
-    start: new Date(2025, 3, 3, 11, 0),
-    end: new Date(2025, 3, 3, 11, 45),
-  },
-
-  {
-    title: "History",
-    allDay: false,
-    start: new Date(2025, 3, 3, 14, 0),
-    end: new Date(2025, 3, 3, 14, 45),
-  },
-  {
-    title: "Math",
-    allDay: false,
-    start: new Date(2025, 3, 4, 8, 0),
-    end: new Date(2025, 3, 4, 8, 45),
-  },
-  {
-    title: "English",
-    allDay: false,
-    start: new Date(2025, 3, 4, 9, 0),
-    end: new Date(2025, 3, 4, 9, 45),
-  },
-
-  {
-    title: "Physics",
-    allDay: false,
-    start: new Date(2025, 3, 4, 11, 0),
-    end: new Date(2025, 3, 4, 11, 45),
-  },
-  {
-    title: "Chemistry",
-    allDay: false,
-    start: new Date(2025, 3, 4, 13, 0),
-    end: new Date(2025, 3, 4, 13, 45),
-  },
-  {
-    title: "History",
-    allDay: false,
-    start: new Date(2025, 3, 4, 14, 0),
-    end: new Date(2025, 3, 4, 14, 45),
-  },
-];
-
 const localizer = momentLocalizer(moment);
-export const BigCalendar = () => {
+export const BigCalendar = ({ events }: { events: IEvent[] }) => {
   const [range, setRange] = useState<View>("work_week");
   const handleChangeView = (view: View) => {
     setRange(view);
@@ -161,7 +16,7 @@ export const BigCalendar = () => {
   return (
     <Calendar
       localizer={localizer}
-      events={calendarEvents}
+      events={events}
       startAccessor="start"
       endAccessor="end"
       views={{ agenda: true, day: true, work_week: true }}
