65bfdd802e7fbdd06a793165d6c4d727520f92f4
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-13 18:16:08 +0300

    feat: models for new better select component and type fix for input component

--- src/utils/models/inputs.ts
+++ src/utils/models/inputs.ts
@@ -12,6 +12,8 @@ export interface IField extends IInput {
   label: string;
   type: string;
   placeholder?: string;
+  defaultValue?: string;
+  props?: React.InputHTMLAttributes<HTMLInputElement>;
 }
 
 export interface ICheckbox
@@ -39,4 +41,23 @@ export interface IMultiSelect {
   placeholder?: string;
   loading?: boolean;
   single?: boolean;
+}
+
+export interface IOption {
+  label: string;
+  value: string | number;
+  color?: string;
+}
+
+export interface ISelectProps {
+  options: IOption[];
+  value: IOption | IOption[] | undefined;
+  onChange: (option: IOption | IOption[] | undefined) => void;
+  placeholder?: string;
+  menuPortalTarget?: HTMLElement;
+  isDisabled?: boolean;
+  isLoading?: boolean;
+  isClearable?: boolean;
+  isSearchable?: boolean;
+  isMulti?: boolean;
 }
