6c24c8ed4c72b81c8b45644a76e678990078aaa9
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-21 12:40:06 +0300

    feat: type file for all responses

--- src/utils/models/response.ts
+++ src/utils/models/response.ts
@@ -1,5 +1,38 @@
+import type { Class, Student, Teacher } from "@prisma/client";
+
 export interface IResponse {
-  success: boolean;
-  error: boolean;
+  isSuccess: boolean;
   message: string;
+}
+export interface ISubject {
+  id: number;
+  name: string;
+}
+export interface IGrade {
+  id: number;
+  level: number;
+}
+export interface IClass extends Class {
+  _count: { students: number };
+}
+
+export interface ISubjectResponse extends IResponse {
+  subjects?: ISubject[];
+}
+export interface IGradesResponse extends IResponse {
+  grades?: IGrade[];
+}
+export interface IClassResponse extends IResponse {
+  classes?: IClass[];
+}
+
+export interface ITeacherResponse extends Teacher {
+  _count: { subjects: number; lessons: number; classes: number };
+  user: { username: string; email: string; avatar: string | null };
+  subjects: { id: number; name: string }[];
+}
+
+export interface IStudentResponse extends Student {
+  user: { username: string; email: string; avatar: string | null };
+  class: Class & { _count: { lessons: number } };
 }
