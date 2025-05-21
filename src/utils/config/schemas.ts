93644023d8ff9fff85064d5bccd765022fc67e6d
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-21 12:29:37 +0300

    feat: fix old schema and add some new
    -added userBaseSchema  for all users schema (changed lastName and firstName to name and surname , +confirmPassword pole)
    -refactor teacherSchema
    -added studentSchema
    -added classSchema

--- src/utils/config/schemas.ts
+++ src/utils/config/schemas.ts
@@ -1,6 +1,8 @@
+import { UserSex } from "@prisma/client";
 import { z } from "zod";
 
-export const teacherSchema = z.object({
+const userBaseSchema = z.object({
+  id: z.string().optional(),
   username: z
     .string()
     .min(5, { message: "Username must be at least 5 characters long" })
@@ -8,27 +10,122 @@ export const teacherSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
   password: z
     .string()
-    .min(5, { message: "Password must be at least 5 characters long" })
+    // .min(5, { message: "Password must be at least 5 characters long" })
     .max(255, { message: "Password must be at most 255 characters long" }),
-  // .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" }),
-  firstName: z
+  confirmPassword: z.string(),
+  name: z
     .string()
     .min(2, { message: "First name must be at least 2 characters long" })
     .max(20, { message: "First name must be at most 20 characters long" }),
-  lastName: z
+  surname: z
     .string()
     .min(2, { message: "Last name must be at least 2 characters long" })
     .max(20, { message: "Last name must be at most 20 characters long" }),
   phone: z
     .string()
-    .min(10, { message: "Phone number must be at least 10 digits long" }),
+    .min(10, { message: "Phone number must be at least 10 digits long" })
+    .optional()
+    .or(z.literal("")),
   address: z.string().min(1, { message: "Address is required" }),
   birthday: z.coerce.date({ message: "Birthday is required" }),
-  sex: z.enum(["male", "female"], { message: "Sex is required" }),
+  sex: z.enum([UserSex.MALE, UserSex.FEMALE], { message: "Sex is required" }),
+  bloodType: z.string().min(1, { message: "Blood Type is required!" }),
   // img: z.instanceof(File, { message: "Image is required" }),
   img: z.string().optional(),
+  formType: z.enum(["Create", "Update"]),
 });
+
+export const teacherSchema = userBaseSchema
+  .extend({ subjects: z.array(z.string()).optional() })
+  .superRefine((data, ctx) => {
+    // Passwords must match
+    if (data.password !== data.confirmPassword) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Passwords do not match",
+        path: ["confirmPassword"],
+      });
+    }
+    // Password is required only for Create
+    if (data.formType === "Create" && !data.password) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Password is required",
+        path: ["password"],
+      });
+    }
+    if (data.formType === "Create" && data.password.length < 5) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Password must be at least 5 characters long",
+        path: ["password"],
+      });
+    }
+  });
+// .refine((data) => data.password === data.confirmPassword, {
+//   message: "Passwords do not match",
+//   path: ["confirmPassword"],
+// });
+// .superRefine((data, ctx) => {
+//   // Passwords must match
+//   if (data.password !== data.confirmPassword) {
+//     ctx.addIssue({
+//       code: z.ZodIssueCode.custom,
+//       message: "Passwords do not match",
+//       path: ["confirmPassword"],
+//     });
+//   }
+//   // Password is required only for Create
+//   if (data.formType === "Create" && !data.password) {
+//     ctx.addIssue({
+//       code: z.ZodIssueCode.custom,
+//       message: "Password is required",
+//       path: ["password"],
+//     });
+//   }
+//   if (data.formType === "Create" && data.password.length < 5) {
+//     ctx.addIssue({
+//       code: z.ZodIssueCode.custom,
+//       message: "Password must be at least 5 characters long",
+//       path: ["password"],
+//     });
+//   }
+// });
 export type TeacherSchema = z.infer<typeof teacherSchema>;
+
+export const studentSchema = userBaseSchema
+  .extend({
+    gradeId: z.coerce.number().min(1, { message: "Grade is required!" }),
+    classId: z.coerce.number().min(1, { message: "Class is required!" }),
+    parentId: z.string().min(1, { message: "Parent Id is required!" }),
+  })
+  .superRefine((data, ctx) => {
+    // Passwords must match
+    if (data.password !== data.confirmPassword) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Passwords do not match",
+        path: ["confirmPassword"],
+      });
+    }
+    // Password is required only for Create
+    if (data.formType === "Create" && !data.password) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Password is required",
+        path: ["password"],
+      });
+    }
+    if (data.formType === "Create" && data.password.length < 5) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Password must be at least 5 characters long",
+        path: ["password"],
+      });
+    }
+  });
+
+export type StudentSchema = z.infer<typeof studentSchema>;
 
 export const signInSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
@@ -46,3 +143,16 @@ export const subjectSchema = z.object({
 });
 
 export type SubjectSchema = z.infer<typeof subjectSchema>;
+
+export const classSchema = z.object({
+  id: z.coerce.number().optional(),
+  name: z.string().min(1, { message: "Class name is required!" }),
+  capacity: z.coerce
+    .number()
+    .min(1, { message: "Class cant be empty!" })
+    .max(30, { message: "Class cant include more than 30 students" }),
+  gradeId: z.coerce.number().min(1, { message: "Grade ID is required" }),
+  supervisorId: z.coerce.string().optional(),
+});
+
+export type ClassSchema = z.infer<typeof classSchema>;
