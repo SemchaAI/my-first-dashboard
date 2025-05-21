fe934a74366d6bb2cc744b376597fc523151e415
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-21 12:56:19 +0300

    refactor: fixing styling of layout because of new form style rules also page is ssr now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-"use client";
-
 import { SignInForm } from "@/components/entities";
 
 export default function Home() {
   return (
     <section className="flex h-dvh w-full flex-col">
-      <h1>Home</h1>
-      <SignInForm />
+      <div className="m-auto w-full max-w-1/3 min-w-80">
+        <SignInForm />
+      </div>
     </section>
   );
 }
