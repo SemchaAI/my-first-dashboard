0f11017ff6f27811d1cbae7bb8b05f4816a11e19
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-21 14:55:41 +0300

    feat(ui): redesign UserCard to display detailed user profile and statistics
    - Added support for nested `children` for modal trigger component, where admin will can update user data

--- src/components/entities/cards/UserCard.tsx
+++ src/components/entities/cards/UserCard.tsx
@@ -1,25 +1,78 @@
-import { Ellipsis } from "lucide-react";
+import Image from "next/image";
+import { Calendar, DropletIcon, Mail, Phone } from "lucide-react";
+import { IUserCard } from "@/utils/models/cards";
+import { InfoCard } from "./InfoCard";
 
 interface IProps {
-  type: string;
-  date: string;
-  total: number;
+  user: IUserCard;
+  children?: React.ReactNode;
 }
 
-export const UserCard = ({ type, date, total }: IProps) => {
+export const UserCard = async ({ user, children }: IProps) => {
   return (
-    <li className="min-w-32 flex-1 rounded-2xl p-4 odd:bg-secondary even:bg-tertiary">
-      <div className="flex grow items-center justify-between">
-        <span className="rounded-full bg-white px-2 py-1 text-xs text-success">
-          {date}
-        </span>
-        <Ellipsis size={24} className="cursor-pointer stroke-white" />
+    <div className="flex flex-col justify-between gap-4 lg:flex-row">
+      {/* teacher card */}
+      <div className="flex flex-1 flex-col gap-4 rounded-md bg-primary px-4 py-6 sm:flex-row">
+        <div className="flex w-full justify-center sm:block sm:w-1/3">
+          <Image
+            src={user.avatar || "/static/img/avatar.png"}
+            alt={user.username}
+            width={144}
+            height={144}
+            className="aspect-square rounded-full object-cover"
+          />
+        </div>
+        <div className="flex w-full flex-col justify-between gap-4 sm:w-2/3">
+          <div>
+            <div className="flex gap-0.5">
+              <h1 className="text-xl font-semibold text-text-highlight">
+                {user.name} {user.surname}
+              </h1>
+              {children}
+            </div>
+
+            <p className="text-sm text-text-primary">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+          <div className="flex flex-wrap justify-between gap-y-2 text-xs font-medium text-text-highlight">
+            <div className="flex grow flex-wrap justify-between">
+              <div className="flex min-w-1/2 items-center gap-1">
+                <DropletIcon
+                  size={14}
+                  className="w-3.5 stroke-text-highlight"
+                />
+                <span>{user.bloodType}</span>
+              </div>
+              <div className="flex min-w-1/2 items-center gap-1">
+                <Calendar size={14} className="w-3.5 stroke-text-highlight" />
+                <span>
+                  {user.birthday.toLocaleString("en-US", {
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </span>
+              </div>
+            </div>
+            <div className="flex grow flex-wrap justify-between">
+              <div className="flex min-w-1/2 items-center gap-1">
+                <Mail size={14} className="min-w-3.5 stroke-text-highlight" />
+                <span>{user.email}</span>
+              </div>
+              <div className="flex min-w-1/2 items-center gap-1">
+                <Phone size={14} className="w-3.5 stroke-text-highlight" />
+                <span>{user.phone}</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1 className="my-4 text-2xl font-bold text-text-highlight">{total}</h1>
-      <p className="text-sm font-medium text-text-primary capitalize">
-        {type}
-        {total > 1 && "s"}
-      </p>
-    </li>
+      {/* statistic cardList */}
+      <div className="flex flex-1 flex-wrap gap-4">
+        {user.statistic.map((statistic) => (
+          <InfoCard key={statistic.title} data={statistic} />
+        ))}
+      </div>
+    </div>
   );
 };
