9b6ae283a9f050c8213df848df74c21a4d3e5f44
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-05-21 14:34:08 +0300

    delete: outdate component MultiSelect

--- src/components/features/index.ts
+++ src/components/features/index.ts
@@ -7,8 +7,6 @@ export { Pagination } from "./pagination/Pagination";
 export { Modal } from "./modal/Modal";
 
 export { InputField } from "./fields/InputField";
-export { MultiSelect } from "./selects/MultiSelect";
-export { MultiSelectField } from "./selects/MultiSelectField";
 export { SelectField } from "./selects/SelectField";
 
 export { UserButton } from "./user/UserButton";
